c6aafaf05f4e3222d87c3a7add6c4ab33ae535cc
Yaouen Vandenbossche <yva@atolcd.com>
2018-07-23 18:05:13 +0200

    Completing the info-drawer componennt
    * Changing alert to snackbar info/warning message
    
    * Incorporating the translation services to the info-drawer component
    
    * Bug fixes
    
    * Commented code

--- src/app/components/info-drawer/info-drawer.component.ts
+++ src/app/components/info-drawer/info-drawer.component.ts
@@ -23,29 +23,43 @@
  * along with Alfresco. If not, see <http://www.gnu.org/licenses/>.
  */
 
-import { Component, Input, OnChanges, SimpleChanges } from '@angular/core';
+import { Component, Input, OnInit, OnChanges, SimpleChanges } from '@angular/core';
 import { MinimalNodeEntity, MinimalNodeEntryEntity } from 'alfresco-js-api';
 import { NodePermissionService } from '../../common/services/node-permission.service';
 import { ContentApiService } from '../../services/content-api.service';
 import { MatSnackBar } from '@angular/material';
+import { TranslationService } from "@alfresco/adf-core";
 
 @Component({
     selector: 'aca-info-drawer',
     templateUrl: './info-drawer.component.html'
 })
-export class InfoDrawerComponent implements OnChanges {
+export class InfoDrawerComponent implements OnInit, OnChanges {
     @Input() nodeId: string;
 
     @Input() node: MinimalNodeEntity;
 
-    @Input() tabLimit: number = 3;
+    @Input() tabLimit = 3;
 
     selectionNodes: SelectionNode[] = new Array();
 
     selectedIndex: number;
 
+    limitWarning: string;
+    folderInfo: string;
+
     isLoading = false;
     displayNode: MinimalNodeEntryEntity;
+
+    // Dès l'initialisation de la vue, chargement de la traduction dans la langue locale
+    ngOnInit() {
+        this.translation.get("APP.INFO_DRAWER.FOLDER_VIEW").subscribe(translation => {
+          this.folderInfo = translation;
+        });
+        this.translation.get("APP.INFO_DRAWER.TAB_LIMIT_WARNING", {limit: this.tabLimit}).subscribe(translation => {
+            this.limitWarning = translation;
+        });
+      }
 
     canUpdateNode(): boolean {
         if (this.displayNode) {
@@ -55,35 +69,57 @@ export class InfoDrawerComponent implements OnChanges {
         return false;
     }
 
+    // Dès que l'utilisateur clique sur un onglet, l'index courant se met à jour.
     setCurrentTab(event: number) {
         this.selectedIndex = event;
     }
 
+    // Ferme l'onglet correspondant à l'index courant.
+    closeTab(){
+        this.selectionNodes.splice(this.selectedIndex, 1);
+    }
+
+    // Met à jour la liste des onglets
     private updateSelectionNodeList(): void {
         const entry = this.node.entry;
+        // Si le document est un fichier, il y a deux cas possibles:
+        // - soit le document est déjà présent dans un onglet
+        // - soit il n'est pas présent
         if (entry.isFile) {
             let alreadyIn: boolean = false;
+            // On regarde si le document est déjà présent dans un onglet
             for (let selectionNode of this.selectionNodes) {
                 if (entry.id == selectionNode.id) {
                     alreadyIn = true;
                     break;
                 }
             }
+            // Si l'onglet correspondant au document n'existe pas :
             if (!alreadyIn) {
+                // On regarde si le nombre d'onglet est à son maximum puis deux cas se présentent :
+                // si on peut encore ajouter un onglet alors on le fait
                 if (!this.isSelectionNodeListFull()) {
-                    this.selectionNodes.unshift({'id': entry.id, 'name': entry.name});
+                    this.selectionNodes.unshift({id: entry.id, name: entry.name});
                     this.selectedIndex = 0;
+                // sinon on affiche un warning prévenant l'utilisateur qu'il est à son maximum d'onglet ouvert
                 } else {
-                    this.snackBar.open("L'affichage est limité à " + this.tabLimit + " prévisualisations simultanées", '', { panelClass: 'warning-snackbar', duration: 2500 });
+                    this.snackBar.open(this.limitWarning, '', {panelClass: 'warning-snackbar', duration: 2500});
                 }
+            // Si l'onglet correspondant au document existe, on replace l'onglet courant sur ce dernier.
             } else {
                 this.selectedIndex = this.selectionNodes.findIndex(selectionNode => selectionNode.id === entry.id);
             }
         } else {
-            this.snackBar.open("Pas de prévisualisation sur un répertoire", '', { panelClass: 'warning-snackbar', duration: 2500 });
+            // On n'affiche pas le message dans deux cas :
+            // - folderInfo n'est pas chargé car le NgOnInit est appelé après le NgOnChange
+            // - l'utilisateur n'est pas sur un onglet de prévisualisation
+            if (this.folderInfo != null && this.selectedIndex <= this.selectionNodes.length - 1) {
+                this.snackBar.open(this.folderInfo, '', {panelClass: 'info-snackbar', duration: 2500});
+            }
         }
     }
 
+    // Renvoie true si le nombre d'onglet est à sa limite
     private isSelectionNodeListFull(): boolean {
         return this.selectionNodes.length >= this.tabLimit;
     }
@@ -103,7 +139,8 @@ export class InfoDrawerComponent implements OnChanges {
     constructor(
         public permission: NodePermissionService,
         private contentApi: ContentApiService,
-        private snackBar: MatSnackBar
+        private snackBar: MatSnackBar,
+        private translation: TranslationService
     ) {}
 
     ngOnChanges(changes: SimpleChanges) {
